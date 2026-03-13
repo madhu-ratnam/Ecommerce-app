208c5c7734ba9565b23bbd1c07051e805e6096a3
Madhu Ratnam <madhu@example.com>
2026-03-14 00:12:58 +0530

    changed 142026

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -8,7 +8,7 @@ const contact = () => {
     <div>
 
       <div className='text-center text-2xl pt-10 border-t'>
-          <Title text1={'CONTACT'} text2={'US'}/>
+        <Title text1={'CONTACT'} text2={'US'} />
       </div>
 
       <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
@@ -16,13 +16,13 @@ const contact = () => {
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
           <p className='text-gray-500'>54709 Willms Station <br /> Suite 350, Washington, USA</p>
-          <p className='text-gray-500'>Tel: (415) 555-0132 <br /> Email: admin@forever.com</p>
-          <p className='font-semibold text-xl text-gray-600'>Carrers at Forever</p>
+          <p className='text-gray-500'>Tel: (415) 555-0132 <br /> Email: admin@marketnext.com</p>
+          <p className='font-semibold text-xl text-gray-600'>Carrers at MarketNext</p>
           <p className='text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
         </div>
       </div>
-      <NewsletterBox/>
+      <NewsletterBox />
     </div>
   )
 }
